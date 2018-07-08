5bb4b2c90ac1b8241e0ebb2cb060ded475c1f8f7
connick <hs.oh@ilts.co.kr>
2018-07-09 02:19:31 +0900

    Test Code

--- src/main/java/com/example/SpringBootBlogApplication.java
+++ src/main/java/com/example/SpringBootBlogApplication.java
@@ -2,11 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootBlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBlogApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello(){
+        return "hello world";
     }
 }
